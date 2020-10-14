e4d96ed54521bc3fd44e46d562ce6aaf33bb9f75
Francesc Torradeflot <torradeflot@pic.es>
2020-10-14 11:37:20 +0200

    Add backup server to anaconda setup

--- scripts/setup_pyspark.sh
+++ scripts/setup_pyspark.sh
@@ -4,20 +4,28 @@ ANACONDA_FILE=Anaconda3-4.2.0-Linux-x86_64.sh
 ANACONDA_LOCAL_FILE=/tmp/$ANACONDA_FILE
 ANACONDA_REMOTE_FILE=https://repo.anaconda.com/archive/$ANACONDA_FILE
 ANACONDA_MD5=4692f716c82deb9fa6b59d78f9f6e85c
+ANACONDA_BKP=https://public.pic.es/s/dyD4bNomgRAQlqC/download
 
 # Download and install Anaconda
 CONDA_VERSION=$(conda --version 2> /dev/null)
 if [ "$CONDA_VERSION" != "conda 4.2.9" ]
 then
-  #curl -o $ANACONDA_LOCAL_FILE $ANACONDA_REMOTE_FILE
   wget -P /tmp $ANACONDA_REMOTE_FILE
   FILE_MD5=$(md5sum $ANACONDA_LOCAL_FILE | awk '{print $1}')
   if [ "$ANACONDA_MD5" != "$FILE_MD5" ]
   then
-      echo "Download of file: $ANACONDA_FILE failed, try again"
-      rm $ANACONDA_FILE
-      exit 1
+      echo "Download of file: $ANACONDA_FILE failed, trying from backup"
+      rm -f $ANACONDA_LOCAL_FILE
+      wget -O $ANACONDA_LOCAL_FILE $ANACONDA_BKP
+      FILE_MD5=$(md5sum $ANACONDA_LOCAL_FILE | awk '{print $1}')
+      if [ "$ANACONDA_MD5" != "$FILE_MD5" ]
+      then
+          echo "Download of file: $ANACONDA_FILE from backup failed, contact support"
+          rm -f $ANACONDA_LOCAL_FILE
+          exit 1
+      fi
   fi
+
   bash $ANACONDA_LOCAL_FILE
   rm -f $ANACONDA_LOCAL_FILE
   
